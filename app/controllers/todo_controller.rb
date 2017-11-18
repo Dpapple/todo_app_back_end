12483d116f63ab3171c877a74a1cf1328e5ee7ea
Danny Pham <danny.chuotcon@gmail.com>
2017-11-18 22:44:12 +0000

    Adding links for show id

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,8 +2,18 @@ class TodoController < ApplicationController
     def index
     end
     def show
-    @todo_description = "Make a curriculum"
-    @todo_pomodoro_estimate = 4
+        @todo_description = "Make a curriculum"
+        @todo_pomodoro_estimate = 4
+        todo_id = params[:id]
+        if todo_id == '1'
+            @todo_description = "Make a curriculum"
+            @todo_pomodoro_estimate = 4
+        elsif todo_id == '2'
+            @todo_description = "Buy workshop supplies"
+            @todo_pomodoro_estimate = 3
+        elsif todo_id == '3'
+            @todo_description = "Meet with volunteer trainers"
+            @todo_pomodoro_estimate = 2
+        end
     end
-    
 end
